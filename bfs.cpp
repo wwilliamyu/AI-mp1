5b50b6d5e8f64500200188c8f20b9df923f6dd19
wwilliamyu <wwilliamyu@gmail.com>
2016-09-22 00:24:56 -0500

    quick fix

--- bfs.cpp
+++ bfs.cpp
@@ -1,18 +1,4 @@
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <fstream>
-#include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-// our own header files
 #include "bfs.h"
-#include "dfs.h"
 #include "cell.h"
 
 int BFS()
